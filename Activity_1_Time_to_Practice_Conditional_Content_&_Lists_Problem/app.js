d4eb95958390dfda9cef42fd5aa6fe834c402f8b
HaizaCasapao <casapaohaiza00@gmail.com>
2021-11-04 20:20:31 -1200

    Topic 10 - Added methods property on a Data vue object

--- Activity_1_Time_to_Practice_Conditional_Content_&_Lists_Problem/app.js
+++ Activity_1_Time_to_Practice_Conditional_Content_&_Lists_Problem/app.js
@@ -7,6 +7,22 @@ const app = Vue.createApp({
         tn: false,
         };
     },
+
+    methods: {
+        addTasks() {
+            this.tasks.push(this.inputTask)
+        },
+        ToggleBtn() {
+            this.tn = !this.tn
+
+            if (this.tn === true) {
+                this.toggler = 'Show List'
+            }
+            else {
+                this.toggler = 'Hide'
+            }
+        }
+    },
 });
 
 app.mount('#assignment');
